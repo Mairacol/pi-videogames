9439cc4e7fcc3e290adcdd359b121533189d3377
Mairacol <120331883+Mairacol@users.noreply.github.com>
2024-01-22 20:47:12 -0300

    api y back conectadas

--- api/src/controllers/createGenres.js
+++
@@ -1,8 +0,0 @@
-//const {Episode} = require ("../db");
-const {Genres} = require("../db");
-
-const createGenres = async(Name) =>  {
-    const newGenres = await Genres.create({ Name });
-    return newGenres;
-}; 
-module.exports = createGenres;
--- api/src/controllers/deleteVideogame.js
+++
@@ -1,9 +0,0 @@
-const {Videogame} = require ("../db");
-
-const deleteVideogame = async(id) =>{
-    const videogame = await Videogame.findByPk(id);
-    await videogame.destroy();
-    return Videogame;
-};
-
-module.exports = deleteVideogame;
